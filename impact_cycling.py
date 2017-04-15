2933d424729eeb651147ebf224c5be9787e50054
Jacob Weightman <jacobdweightman@gmail.com>
2017-04-15 09:56:53 -0500

    added home screen

--- impact_cycling.py
+++ impact_cycling.py
@@ -16,7 +16,7 @@ engine = create_engine('sqlite:///tutorial.db', echo=True)
 
 @app.route('/')
 def get_main_page():
-    return flask.render_template('test-index.html')
+    return flask.render_template('index.html')
 
 @app.route('/about')
 def about():
@@ -55,15 +55,15 @@ def logout():
 @app.route("/profile/")
 def profile():
     POST_USERNAME = str(request.form['username'])
-    
+
     user_data = [POST_USERNAME]
-    
+
     return flask.render_template('profile.html', userData = user_data)
-    
+
 
 @app.route('/logtrip/')
 def log_trip():
-    past_trips = trip_log.get_past_trips()    
+    past_trips = trip_log.get_past_trips()
 
     Session = sessionmaker(bind=engine)
     s = Session()
@@ -75,37 +75,37 @@ def log_trip():
 
 @app.route('/tripdata/', methods=['POST'])
 def trip_data():
-    
+
     api_key = 'AIzaSyCgL4EhbainFOaUs3OJDUasN_9X3Kv7CN0'
-    
+
     startpoint = request.form['startpoint']
     endpoint = request.form['endpoint']
-    
+
     startpoint_url = startpoint.replace(" ", "+")
     endpoint_url = endpoint.replace(" ", "+")
-    
+
     urlstring1 = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + startpoint_url + "&key=" +api_key
     start_info = simplejson.load(urlopen(urlstring1))
     start_coordinate = [start_info['results'][0].get("geometry").get("location").get("lat"), start_info['results'][0].get("geometry").get("location").get("lng")]
-    
+
     urlstring2 = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + endpoint_url + "&key=" + api_key
     end_info = simplejson.load(urlopen(urlstring2))
     end_coordinate = [end_info['results'][0].get("geometry").get("location").get("lat"), end_info['results'][0].get("geometry").get("location").get("lng")]
-    
-    
+
+
     distance = vincenty(start_coordinate, end_coordinate).miles
-    
+
     co2 = 411 * distance
-    
+
     #avg mpg: 25.5
     #avg price per gallon: 2.42
     gallons_used = distance / 25.5
-    money_saved = 2.42 * gallons_used 
+    money_saved = 2.42 * gallons_used
 
     print(distance)
-    
+
     points = [startpoint, endpoint, distance, co2, money_saved]
-    
+
     return flask.render_template('trip-data.html', points = points)
 
 
