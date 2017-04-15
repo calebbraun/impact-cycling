4831edfc85bf6821efa5682cb6621f6c760b4193
Caleb Braun <Caleb@eduroam-164-218.dyn.carleton.edu>
2017-04-15 13:54:14 -0500

    fixed regester page

--- generateTable.py
+++ generateTable.py
@@ -4,7 +4,8 @@ import calendar
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from tabledef import *
+from models import *
+from impact_cycling import db
 
 engine = create_engine('sqlite:///tutorial.db')
 
@@ -13,6 +14,7 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 user = User(username="python", password="python", first_name="Dustin")
+trip = Trips(lat=12.0, lon=140.34, distance=2, date=datetime.datetime.now(), user_id=1)
 session.add(user)
 
 timestamp1 = datetime(2017, 4, 12)
