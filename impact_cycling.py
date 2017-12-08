f4058652b24c5d70b82b2dc25fc0b2202adf652d
calebbraun <calebjbraun@gmail.com>
2017-12-07 23:15:34 -0500

    Start turning into real project

--- impact_cycling.py
+++ impact_cycling.py
@@ -1,14 +1,13 @@
 import flask
 import datetime
-import simplejson
-from urllib2 import urlopen
-#from urllib.request import urlopen
+import json
+# from urllib2 import urlopen
+from urllib.request import urlopen
 from flask import Flask, flash, jsonify, render_template, request, session
 from flask_googlemaps import GoogleMaps, Map
 from geopy.distance import vincenty
 from models import *
 import trip_log
-from sqlalchemy.sql import and_
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.secret_key = 'DYF~KPCVVjkdfFEQ93jJ]'
@@ -88,31 +87,31 @@ def logout():
 
 @app.route("/profile/")
 def profile():
-    
+
     username = session['username']
     name = User.query.filter_by(username=username).first().first_name
     userid = User.query.filter_by(username=username).first().id
-    
-    
+
+
     qry = Trips.query.filter_by(user_id=userid).count()
-    
+
 
     distance = 0
-    
+
     for i in range(1,qry+1):
         qry2 = Trips.query.filter_by(id=i).first().distance
         print(qry2)
         distance += qry2
-        
+
     gallons_used = distance / 25.5
     money_saved = 2.42 * gallons_used
     co2 = distance * 411
-    
+
     distance = "{0:.2f}".format(distance)
     gallons_used = "{0:.2f}".format(gallons_used)
     money_saved = "{0:.2f}".format(money_saved)
     co2 = "{0:.2f}".format(co2)
-    
+
     user_data = [name, distance, money_saved, co2]
 
     return flask.render_template('profile.html', userData = user_data)
@@ -135,6 +134,8 @@ def trip_data():
     endpoint_url = endpoint.replace(" ", "+")
 
     urlstring1 = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + startpoint_url + "&key=" +api_key
+    print(urlopen(urlstring1))
+
     start_info = simplejson.load(urlopen(urlstring1))
     start_lat = start_info['results'][0].get("geometry").get("location").get("lat")
     start_lon = start_info['results'][0].get("geometry").get("location").get("lng")
